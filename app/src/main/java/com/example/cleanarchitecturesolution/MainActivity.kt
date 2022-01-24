34ea7da3acdfb7e66e8cc7220cd7d9a90db421d7
Mateusz Mroz <mateusz.mroz10@gmail.com>
2022-01-24 21:43:39 +0100

    Add app di module

--- app/src/main/java/com/example/cleanarchitecturesolution/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecturesolution/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.cleanarchitecturesolution
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.koin.android.scope.AndroidScopeComponent
+import org.koin.androidx.scope.activityRetainedScope
+import org.koin.core.scope.Scope
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AndroidScopeComponent {
+
+    override val scope: Scope by activityRetainedScope()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
